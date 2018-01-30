b9bc0aee58939060e5bec2ee59e16faedaa40866
David <djlowes@gmail.com>
2018-01-30 14:34:39 -0800

    Update shortest-word.js

--- Javascript/shortest-word.js
+++ Javascript/shortest-word.js
@@ -9,3 +9,9 @@ function findShort(s) {
   return arr[0].length
 }
 
+// Sample Tests
+Test.describe("Example tests",_=>{
+Test.assertEquals(findShort("bitcoin take over the world maybe who knows perhaps"), 3);
+Test.assertEquals(findShort("turns out random test cases are easier than writing out basic ones"), 3); 
+});
+
