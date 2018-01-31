487213ab299e8a389bf9d8c4555b078f2fc493ac
David <djlowes@gmail.com>
2018-01-31 15:38:11 -0800

    Update bubble-sort.js

--- Javascript/bubble-sort.js
+++ Javascript/bubble-sort.js
@@ -1,3 +1,24 @@
+// Task 
+// Given an -element array, , of distinct elements, sort array  in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
+
+// Array is sorted in numSwaps swaps., where  is the number of swaps that took place.
+// First Element: firstElement, where  is the first element in the sorted array.
+// Last Element: lastElement, where  is the last element in the sorted array.
+// Hint: To complete this challenge, you must add a variable that keeps a running tally of all swaps that occur during execution.
+
+// Input Format
+// The first line contains an integer, , denoting the number of elements in array . 
+// The second line contains  space-separated integers describing the respective values of .
+
+// Output Format
+// You must print the following three lines of output:
+
+// Array is sorted in numSwaps swaps., where  is the number of swaps that took place.
+// First Element: firstElement, where  is the first element in the sorted array.
+// Last Element: lastElement, where  is the last element in the sorted array.
+
+
+
 process.stdin.resume();
 process.stdin.setEncoding('ascii');
 
